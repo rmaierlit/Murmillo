a2e64ae518cfb0c9208f66e914e7f380210e0140
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-01-03 15:31:54 -0800

    (test) set up test framework for reducers
    also, jshint config file has been changed to accept es6

--- test/reduxSpec.js
+++ test/reduxSpec.js
@@ -1,4 +1,3 @@
-'use strict';
 /*global describe, it*/
 
 import { setUser, updateUser } from '../client/src/actions/actions.js';
@@ -6,18 +5,26 @@ var expect = require('chai').expect;
 
 describe('actions', function() {
   describe('setUser', function() {
-    it('should create an action', function() {
+    it('should create a SET_USER action', function() {
       var action = setUser({name: 'jeff bridges'});
       //should create {type: SET_USER, user: {name: 'jeff bridges'}}
       expect(action.type).to.equal('SET_USER');
       expect(action.user.name).to.equal('jeff bridges');
-    });
-  });
+    })
+  })
   describe('updateUser', function() {
-    it('should update the user', function() {
+    it('should create a UPDATE_USER action', function() {
       var action = updateUser({score: 0});
       expect(action.type).to.equal('UPDATE_USER');
       expect(action.user.score).to.equal(0);
-    });
-  });
-});
+    })
+  })
+})
+
+describe('reducers', function() {
+  describe('user', function() {
+    it('should record a new user in the state', function() {
+      expect(false).to.equal(true);
+    })
+  })
+})
