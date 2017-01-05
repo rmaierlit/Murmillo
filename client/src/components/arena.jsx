c16f063b672f11d3bd137bb6e6c316d7f7a13184
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-01-04 16:41:10 -0800

    feat(editor) sets up react-ace editor
    editor is on page '/arena'

--- client/src/components/arena.jsx
+++ client/src/components/arena.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Editor from './editor.jsx';
 
 const Arena = (props) => (
   <div>
     <h1>I'm the arena</h1>
+    <Editor />
   </div>
 );
 
--- client/src/components/editor.jsx
+++ client/src/components/editor.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import brace from 'brace';
+import AceEditor from 'react-ace';
 
-class Editor extends react.Component {
+import 'brace/mode/java';
+import 'brace/theme/github';
+
+
+class Editor extends React.Component {
   render() {
     return(
-      <div id="editor">
-
-      </div>
+      <AceEditor
+        mode="javascript"
+        theme="chrome"
+        name="code"
+        width="100%"
+        minLines={3}
+        maxLines={50}
+        ref="ace"
+        fontSize={18}
+        value="//type your code here"
+      />
     );
   }
 }
